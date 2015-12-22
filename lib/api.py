4a4ba146d397f9831414a8d56eb9d97d22af04d3
Aaeronn <aaeronn.bhatta@gmail.com>
2015-12-22 15:41:52 +0545

    api request class

--- lib/api.py
+++ lib/api.py
@@ -1,46 +1,49 @@
 import requests, json, sys
 from lib import urls, constants, exceptions
-#from lib.session import is_logged_in
+from lib.auth.auth import picovico_auth_headers
+# from auth.auth import get_auth_headers
+#0from lib.session import is_logged_in
+
 
 class PicovicoAPIRequest:
 
-	def is_logged_in(self, auth_session):
-		try:
-			if auth_session.get('X-Access-Key') and auth_session.get('X-Access-Token'):
-				return True
-		except:
-			return None
+	# def is_logged_in(self, auth_session):
+	# 	try:
+	# 		if auth_session.get('X-Access-Key') and auth_session.get('X-Access-Token'):
+	# 			return True
+	# 	except:
+	# 		return None
 
-		return False
+	# 	return False
 
-	def get_auth_headers(self, is_anonymous, auth_session):
-		'''
-		Picovico: Checks if user is anonymous and returns exceptis if it is.
-		'''
-		if is_anonymous:
-			return {
-			"X-Access-Key":None,
-			"X-Access-Token":None
-		}
-		if not is_anonymous and not self.is_logged_in(auth_session):
-			raise exceptions.PicovicoUnauthorizedException()
+	# def get_auth_headers(self, is_anonymous, auth_session):
+	# 	'''
+	# 	Picovico: Checks if user is anonymous and returns exceptis if it is.
+	# 	'''
+	# 	if is_anonymous:
+	# 		return {
+	# 		"X-Access-Key":None,
+	# 		"X-Access-Token":None
+	# 	}
+	# 	if not is_anonymous and not self.is_logged_in(auth_session):
+	# 		raise exceptions.PicovicoUnauthorizedException()
 
-		return auth_session
+	# 	return auth_session
 
-	def get(self, url=None, is_anonymous=False, auth_session=None):
-		response = requests.get(urls.PICOVICO_API_ENDPOINT + url, headers=self.get_auth_headers(is_anonymous, auth_session))
+	def get(self, url=None, headers=None):
+		response = requests.get(urls.PICOVICO_API_ENDPOINT + url, headers=headers)
 		return self.sdk_response(response)
 
-	def post(self, url=None, data=None, is_anonymous=False, auth_session=None):
-		response = requests.post(urls.PICOVICO_API_ENDPOINT + url, data, headers=self.get_auth_headers(is_anonymous, auth_session))
+	def post(self, url=None, data=None, headers=None):
+		response = requests.post(urls.PICOVICO_API_ENDPOINT + url, data, headers=headers)
 		return self.sdk_response(response)
 
-	def put(self, url=None, filename=None, data=None, is_anonymous=False, auth_session=None):
-		response = requests.put(urls.PICOVICO_API_ENDPOINT + url, filename, data, headers=self.get_auth_headers(is_anonymous, auth_session))
+	def put(self, url=None, filename=None, data=None, headers=None):
+		response = requests.put(urls.PICOVICO_API_ENDPOINT + url, filename, data, headers=headers)
 		return self.sdk_response(response)
 
-	def delete(self, url=None, is_anonymous=False, auth_session=None):
-		response = requests.delete(urls.PICOVICO_API_ENDPOINT + url, headers=self.get_auth_headers(is_anonymous, auth_session))
+	def delete(self, url=None, headers=None):
+		response = requests.delete(urls.PICOVICO_API_ENDPOINT + url, headers=headers)
 		return self.sdk_response(response)
 
 	def sdk_response(self, response):
