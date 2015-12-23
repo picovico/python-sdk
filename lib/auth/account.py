d4db5f2f55fa6b3939429defa94caa2b1302a83b
Aaeronn <aaeronn.bhatta@gmail.com>
2015-12-23 09:54:05 +0545

    session required exception message added

--- lib/auth/account.py
+++ lib/auth/account.py
@@ -2,15 +2,19 @@ from lib import urls
 from lib.auth.session import PicovicoSession
 from lib.api import PicovicoAPIRequest
 from lib.exceptions import PicovicoSessionRequiredException
+from lib.messages import SESSION_REQUIRED_MESSAGE
+
 
 class PicovicoAccount():
 
 	picovico_session = None
 
 	def __init__(self, picovico_session):
-		if picovico_session is None:
-			raise PicovicoSessionRequiredException()
-		self.picovico_session = picovico_session
+		try:
+			if picovico_session:
+				self.picovico_session = picovico_session
+		except:
+			raise PicovicoSessionRequiredException(SESSION_REQUIRED_MESSAGE)
 
 	def profile(self):
 		'''
--- lib/messages.py
+++ lib/messages.py
@@ -4,3 +4,4 @@ Picovico messages
 ACCESS_KEY_AND_ACCESS_TOKEN_MISSING = "Access key and access token missing"
 VIDEO_ID_NOT_FOUND = "Video ID not found."
 VIDEO_DATA_NOT_FOUND = "Video data not found"
+SESSION_REQUIRED_MESSAGE = "Session required"
