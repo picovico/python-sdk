d8bf9b5280371f72ae33ac100e4f1c0dd153a571
Aaeronn <aaeronn.bhatta@gmail.com>
2015-12-28 17:43:56 +0545

    subclassing project class

--- project.py
+++ project.py
@@ -8,138 +8,138 @@ from lib.components.style import PicovicoStyle
 from lib.exceptions import PicovicoSessionRequiredException
 from lib.messages import SESSION_REQUIRED_MESSAGE
 
-class PicovicoHelpers(object):
+# class PicovicoHelpers(object):
 
-	def add_library_image(self, image_id, vdd, caption=""):
-		'''
-			Picovico: Appends any image previously uploaded.
-		'''
-		if image_id:
-			self.append_image_slide(image_id, vdd, caption)
-			return True
+# 	def add_library_image(self, image_id, vdd, caption=""):
+# 		'''
+# 			Picovico: Appends any image previously uploaded.
+# 		'''
+# 		if image_id:
+# 			self.append_image_slide(image_id, vdd, caption)
+# 			return True
 
-		return False
+# 		return False
 
-	def append_image_slide(self, image_id, vdd, caption=None):
-		'''
-			Picovico: Appends image slide with given data
-		'''
-		data = {
-			'name': 'image',
-			'data':{
-				'text': caption,
-			},
-			'asset_id': image_id
-		}
-		self.append_vdd_slide(vdd, data)
+# 	def append_image_slide(self, image_id, vdd, caption=None):
+# 		'''
+# 			Picovico: Appends image slide with given data
+# 		'''
+# 		data = {
+# 			'name': 'image',
+# 			'data':{
+# 				'text': caption,
+# 			},
+# 			'asset_id': image_id
+# 		}
+# 		self.append_vdd_slide(vdd, data)
 
-	def append_vdd_slide(self, vdd, slide):
-		'''
-			Picovico: Appends image slides into the project with data
-		'''
-		if vdd:
-			if not vdd['assets']:
-				vdd['assets'] = []
+# 	def append_vdd_slide(self, vdd, slide):
+# 		'''
+# 			Picovico: Appends image slides into the project with data
+# 		'''
+# 		if vdd:
+# 			if not vdd['assets']:
+# 				vdd['assets'] = []
 
-			last_slide = None
-			current_slides_count = len(vdd['assets'])
-			last_end_time = 0
+# 			last_slide = None
+# 			current_slides_count = len(vdd['assets'])
+# 			last_end_time = 0
 
-			if vdd['assets']:
-				last_slide = vdd['assets'][len(vdd['assets']) - 1]
+# 			if vdd['assets']:
+# 				last_slide = vdd['assets'][len(vdd['assets']) - 1]
 
-				if last_slide:
-					last_end_time = last_slide["end_time"]
-				else:
-					last_end_time = last_slide.end_time
+# 				if last_slide:
+# 					last_end_time = last_slide["end_time"]
+# 				else:
+# 					last_end_time = last_slide.end_time
 
-			slide['start_time'] = last_end_time
-			slide['end_time'] = last_end_time + constants.STANDARD_SLIDE_DURATION
+# 			slide['start_time'] = last_end_time
+# 			slide['end_time'] = last_end_time + constants.STANDARD_SLIDE_DURATION
 
-			vdd['assets'].append(slide)
-			return vdd
+# 			vdd['assets'].append(slide)
+# 			return vdd
 
-	def append_text_slide(self, vdd, title=None, text=None):
-		'''
-			Picovico: Prepares the slide data for text slides and appends to the vdd
-		'''
-		data = {
-			'name': 'text',
-			'data':{
-				'title': title,
-				'text': text
-			}
-		}
-		self.append_vdd_slide(vdd, data)
+# 	def append_text_slide(self, vdd, title=None, text=None):
+# 		'''
+# 			Picovico: Prepares the slide data for text slides and appends to the vdd
+# 		'''
+# 		data = {
+# 			'name': 'text',
+# 			'data':{
+# 				'title': title,
+# 				'text': text
+# 			}
+# 		}
+# 		self.append_vdd_slide(vdd, data)
 
-	def add_library_music(self, music_id, vdd):
-		'''
-			Picovico: Define any previously uploaded music, or any music available from library. 
-		'''
-		if music_id:
-			self.set_music(music_id, vdd)
-			return True
+# 	def add_library_music(self, music_id, vdd):
+# 		'''
+# 			Picovico: Define any previously uploaded music, or any music available from library. 
+# 		'''
+# 		if music_id:
+# 			self.set_music(music_id, vdd)
+# 			return True
 
-		return False
+# 		return False
 
-	def set_music(self, music_id, vdd):
-		'''
-			Picovico:
-				Saves music for the current video project.
-				Saved separately because only one music is supported.
-		'''
-		data = {
-			'name': 'music',
-			'asset_id': music_id,
-			'_comment': 'Some cool comment which will replace later'
-		}
-		vdd['_music'] = data
-		self.append_vdd_slide(vdd, vdd['_music'])
-		del vdd['_music']
+# 	def set_music(self, music_id, vdd):
+# 		'''
+# 			Picovico:
+# 				Saves music for the current video project.
+# 				Saved separately because only one music is supported.
+# 		'''
+# 		data = {
+# 			'name': 'music',
+# 			'asset_id': music_id,
+# 			'_comment': 'Some cool comment which will replace later'
+# 		}
+# 		vdd['_music'] = data
+# 		self.append_vdd_slide(vdd, vdd['_music'])
+# 		del vdd['_music']
 
-	def save(self, video_id):
-		'''
-			Picovico: Save the current progress with the project.
-		'''
-		#self.append_music(self.vdd)
+# 	def save(self, video_id):
+# 		'''
+# 			Picovico: Save the current progress with the project.
+# 		'''
+# 		#self.append_music(self.vdd)
 
-		video_assets = {}
-		for k,v in self.vdd.items():
-			if type(v) is list:
-				video_assets[k] = json.dumps(v)
-			else:
-				video_assets[k] = v
+# 		video_assets = {}
+# 		for k,v in self.vdd.items():
+# 			if type(v) is list:
+# 				video_assets[k] = json.dumps(v)
+# 			else:
+# 				video_assets[k] = v
 
-		response = PicovicoAPIRequest.post((urls.SAVE_VIDEO).format(video_id), data=video_assets, headers=self.auth_headers)
-		return response
+# 		response = PicovicoAPIRequest.post((urls.SAVE_VIDEO).format(video_id), data=video_assets, headers=self.auth_headers)
+# 		return response
 
-	def reset_slides(self, vdd):
-		'''
-			Picovico: Resets slides data i.e assets
-		'''
-		vdd['assets'] = []
+# 	def reset_slides(self, vdd):
+# 		'''
+# 			Picovico: Resets slides data i.e assets
+# 		'''
+# 		vdd['assets'] = []
 
-	def reset_music(self, vdd):
-		'''
-			Picovico: Resets music for the project
-		'''
-		try:
-			if vdd['_music']:
-				del vdd['_music']
-		except KeyError:
-			pass
+# 	def reset_music(self, vdd):
+# 		'''
+# 			Picovico: Resets music for the project
+# 		'''
+# 		try:
+# 			if vdd['_music']:
+# 				del vdd['_music']
+# 		except KeyError:
+# 			pass
 
-	# def append_music(self, vdd):
-	# 	'''
-	# 		Picovico: If music is set and not appended to the VDD slide, appends the music as vdd slide
-	# 	'''
-	# 	if vdd['_music']:
-	# 		self.append_vdd_slide(vdd, vdd['_music'])
-	# 		del vdd['_music']
+# 	# def append_music(self, vdd):
+# 	# 	'''
+# 	# 		Picovico: If music is set and not appended to the VDD slide, appends the music as vdd slide
+# 	# 	'''
+# 	# 	if vdd['_music']:
+# 	# 		self.append_vdd_slide(vdd, vdd['_music'])
+# 	# 		del vdd['_music']
 
 
 
-class PicovicoProject(PicovicoVideo, PicovicoHelpers):
+class PicovicoProject(PicovicoVideo):
 
 	picovico_session = None
 
@@ -212,7 +212,7 @@ class PicovicoProject(PicovicoVideo, PicovicoHelpers):
 		response = self.pv_photo.upload_image(image_path, source)
 
 		if response['id']:
-			self.add_library_image(response['id'], self.vdd, caption)
+			self.ProjectHelpers(self).add_library_image(response['id'], self.vdd, caption)
 
 		return response
 
@@ -221,7 +221,7 @@ class PicovicoProject(PicovicoVideo, PicovicoHelpers):
 			Picovico: Adds text slide to the project
 		'''
 		if title or text:
-			self.append_text_slide(self.vdd, title, text)
+			PicovicoProject.ProjectHelpers(self).append_text_slide(self.vdd, title, text)
 			return True
 		
 		return False
@@ -233,7 +233,7 @@ class PicovicoProject(PicovicoVideo, PicovicoHelpers):
 		response = self.pv_music.upload_music(music_path)
 
 		if response['id']:
-			self.add_library_music(response['id'], self.vdd)
+			PicovicoProject.ProjectHelpers(self).add_library_music(response['id'], self.vdd)
 
 		return response
 
@@ -282,11 +282,11 @@ class PicovicoProject(PicovicoVideo, PicovicoHelpers):
 		'''
 			Picovico: Resets the current local progress
 		'''
-		self.reset_music(self.vdd)
-		self.reset_slides(self.vdd)
-		self.remove_credits()
-		self.vdd['style'] = None
-		self.vdd['quality'] = None
+		PicovicoProject.ProjectHelpers(self).reset_music(self.vdd)
+		PicovicoProject.ProjectHelpers(self).reset_slides(self.vdd)
+		PicovicoProject.ProjectHelpers(self).remove_credits()
+		PicovicoProject.ProjectHelpers(self).vdd['style'] = None
+		PicovicoProject.ProjectHelpers(self).vdd['quality'] = None
 		return self.vdd
 
 	def set_quality(self, quality):
@@ -298,6 +298,145 @@ class PicovicoProject(PicovicoVideo, PicovicoHelpers):
 			return True
 			
 		return False
+
+	class ProjectHelpers():
+
+		def __init__(self, project_instance):
+			self.project_helpers = project_instance
+			print("wow")
+			print(self.project_helpers)
+			print(self.project_helpers.vdd)
+
+		def add_library_image(self, image_id, vdd, caption=""):
+			'''
+			Picovico: Appends any image previously uploaded.
+			'''
+			if image_id:
+				self.append_image_slide(image_id, vdd, caption)
+				return True
+
+			return False
+
+		def append_image_slide(self, image_id, vdd, caption=None):
+			'''
+			Picovico: Appends image slide with given data
+			'''
+			data = {
+				'name': 'image',
+				'data':{
+					'text': caption,
+				},
+				'asset_id': image_id
+			}
+			self.append_vdd_slide(vdd, data)
+
+		def append_vdd_slide(self, vdd, slide):
+			'''
+				Picovico: Appends image slides into the project with data
+			'''
+			if vdd:
+				if not vdd['assets']:
+					vdd['assets'] = []
+
+				last_slide = None
+				current_slides_count = len(vdd['assets'])
+				last_end_time = 0
+
+				if vdd['assets']:
+					last_slide = vdd['assets'][len(vdd['assets']) - 1]
+
+					if last_slide:
+						last_end_time = last_slide["end_time"]
+					else:
+						last_end_time = last_slide.end_time
+
+				slide['start_time'] = last_end_time
+				slide['end_time'] = last_end_time + constants.STANDARD_SLIDE_DURATION
+
+				vdd['assets'].append(slide)
+				return vdd
+
+		def append_text_slide(self, vdd, title=None, text=None):
+			'''
+				Picovico: Prepares the slide data for text slides and appends to the vdd
+			'''
+			data = {
+				'name': 'text',
+				'data':{
+					'title': title,
+					'text': text
+				}
+			}
+			self.append_vdd_slide(vdd, data)
+
+		def add_library_music(self, music_id, vdd):
+			'''
+				Picovico: Define any previously uploaded music, or any music available from library. 
+			'''
+			if music_id:
+				self.set_music(music_id, vdd)
+				return True
+
+			return False
+
+		def set_music(self, music_id, vdd):
+			'''
+				Picovico:
+					Saves music for the current video project.
+					Saved separately because only one music is supported.
+			'''
+			data = {
+				'name': 'music',
+				'asset_id': music_id,
+				'_comment': 'Some cool comment which will replace later'
+			}
+			vdd['_music'] = data
+			self.append_vdd_slide(vdd, vdd['_music'])
+			del vdd['_music']
+
+		def save(self, video_id):
+			'''
+				Picovico: Save the current progress with the project.
+			'''
+			#self.append_music(self.vdd)
+
+			video_assets = {}
+			for k,v in self.project_helpers.vdd.items():
+				if type(v) is list:
+					video_assets[k] = json.dumps(v)
+				else:
+					video_assets[k] = v
+
+			response = PicovicoAPIRequest.post((urls.SAVE_VIDEO).format(video_id), data=video_assets, headers=self.project_helpers.auth_headers)
+			return response
+
+		def reset_slides(self, vdd):
+			'''
+				Picovico: Resets slides data i.e assets
+			'''
+			vdd['assets'] = []
+
+		def reset_music(self, vdd):
+			'''
+				Picovico: Resets music for the project
+			'''
+			try:
+				if vdd['_music']:
+					del vdd['_music']
+			except KeyError:
+				pass
+
+		# def append_music(self, vdd):
+		# 	'''
+		# 		Picovico: If music is set and not appended to the VDD slide, appends the music as vdd slide
+		# 	'''
+		# 	if vdd['_music']:
+		# 		self.append_vdd_slide(vdd, vdd['_music'])
+		# 		del vdd['_music']
+
+	
+
+
 
 
 
