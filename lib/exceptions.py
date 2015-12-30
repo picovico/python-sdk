2e5c912f0f6cb27b5859417755f53aab7ea74d6f
Aaeronn <aaeronn.bhatta@gmail.com>
2015-12-30 10:56:26 +0545

    comments on exceptions class

--- lib/exceptions.py
+++ lib/exceptions.py
@@ -1,12 +1,13 @@
-"""
+'''
 	Picovico: Picovico custom exception classes
-"""
-"""
-Picovico:
-	When the access_key and access_token aren't available
-	for the curent session
-"""
+'''
+
 class PicovicoUnauthorizedException(Exception):
+	'''
+		Picovico:
+				When the access_key and access_token aren't available
+				for the curent session
+	'''
 
 	def __init__(self, message=None):
 		self.message = "Not Authorised"
@@ -14,18 +15,18 @@ class PicovicoUnauthorizedException(Exception):
 	def __str__(self):
 		return repr(self.message)
 
-"""
-Picovico:
-	When the response status code is not 200
-	Something like : 
-	{
-		"error":{
-			'status': 400,
-			'message': "Some messages"
-		}
-	}
-"""
 class PicovicoAPIResponseException(Exception):
+	'''
+		Picovico:
+				When the api response status code is not 200
+				Error should be something like : 
+								{
+								"error":{
+										'status': 400,
+										'message': "Some messages"
+									}
+							}
+	'''
 
 	def __init__(self, status=None, message=None, data=None):
 		self.status = status
@@ -36,11 +37,20 @@ class PicovicoAPIResponseException(Exception):
 		return  repr({'error':{'status':self.status,'message': self.message}, 'data': self.data})
 
 class DataNotFound(Exception):
+	'''
+		Picovico: Exception to be raised if expected data is not found.
+	'''
 	pass
 
 class VideoIdNotFound(Exception):
+	'''
+		Picovico: Exception to be raised if video id is not found.
+	'''
 	pass
 
 class PicovicoSessionRequiredException(Exception):
+	'''
+		Picovico: Exception to be raised if Picovico session instance is not provided.
+	'''
 	pass
 	
