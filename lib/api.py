86b08f72bfb355ce1543a8b10c2793b6bfae429b
Aaeronn <aaeronn.bhatta@gmail.com>
2015-12-30 10:28:34 +0545

    added comment to the PicovicoAPIRequest class and methods

--- lib/api.py
+++ lib/api.py
@@ -3,19 +3,31 @@ from lib import urls, constants, exceptions
 
 
 class PicovicoAPIRequest:
+	'''
+		Picovico: Picovico API Request methods.
+	'''
 
 	@staticmethod
 	def get(url=None, headers=None):
+		'''
+			Picovico: Performs get request. #GET()
+		'''
 		response = requests.get(urls.PICOVICO_API_ENDPOINT + url, headers=headers)
 		return PicovicoAPIRequest.sdk_response(response)
 
 	@staticmethod
 	def post(url=None, data=None, headers=None):
+		'''
+			Picovico: Performs post request. #POST()
+		'''
 		response = requests.post(urls.PICOVICO_API_ENDPOINT + url, data, headers=headers)
 		return PicovicoAPIRequest.sdk_response(response)
 
 	@staticmethod
 	def put(url=None, filename=None, data=None, headers=None):
+		'''
+			Picovico: Performs put request. #PUT()
+		'''
 		pv_file = open(filename, 'r')
 		if data:
 			headers.update(data)
@@ -24,11 +36,19 @@ class PicovicoAPIRequest:
 
 	@staticmethod
 	def delete(url=None, headers=None):
+		'''
+			Picovico: Performs delete request. #DELETE()
+		'''
 		response = requests.delete(urls.PICOVICO_API_ENDPOINT + url, headers=headers)
 		return PicovicoAPIRequest.sdk_response(response)
 
 	@staticmethod
 	def sdk_response(raw_response):
+		'''
+			Picovico: Decodes the raw response from api to json.
+						Checks if status code is 200. If not raises PicovicoAPIResponseException with error message.
+						Returns decoded response
+		'''
 		decoded_response = json.loads(raw_response.text)
 
 		if not raw_response.status_code == 200:
