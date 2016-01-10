4eebaab495f0ba2bb483eed83c429f48116c699b
acpmasquerade <d@picovico.com>
2016-01-10 12:52:41 +0545

    cleanup - bump 2.0.4

---
+++ lib/config.sample.py
@@ -0,0 +1,6 @@
+'''
+        Picovico configuration for APP_ID, APP_SECRET and DEVICE_ID.
+'''
+PICOVICO_DEVICE_ID = 'com.picovico.api.python-sdk'
+PICOVICO_APP_ID = "some-app-id"
+PICOVICO_APP_SECRET = "some-app-secret"
