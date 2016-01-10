ad028b75b47ca0c7f77b42e6ed537048f3aca3ff
acpmasquerade <d@picovico.com>
2016-01-10 13:10:41 +0545

    bump 2.0.6 - setup.py fix for pip install

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.5',
+    version='2.0.6',
 
     description='Official Python SDK for Picovico Developer API',
     long_description=long_description,
@@ -94,19 +94,22 @@ setup(
     # installed, specify them here.  If using Python 2.6 or less, then these
     # have to be included in MANIFEST.in as well.
     package_data={
-        'sample': ['package_data.dat'],
+        #'sample': ['package_data.dat'],
     },
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
     # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    data_files=[('my_data', ['data/data_file'])],
+
+    #data_files=[('my_data', ['data/data_file'])],
+	data_files=[],
+
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # pip to create the appropriate form of executable for the target platform.
     entry_points={
-        'console_scripts': [
-            'sample=sample:main',
-        ],
+        #'console_scripts': [
+        #    'sample=sample:main',
+        #],
     },
 )
