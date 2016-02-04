76eb354777dc38426a480118d041f980c4843346
Sagar Chalise <chalisesagar@gmail.com>
2016-02-04 21:10:29 +0545

    Url and thumbnail/preview upload  functionality

--- picovico/components/music.py
+++ picovico/components/music.py
@@ -5,3 +5,6 @@ class PicovicoMusic(PicovicoBaseComponent):
     """ Picovico Music Component Class """
     def __init__(self, request_obj):
         super(PicovicoMusic, self).__init__(request_obj, 'music')
+
+    def _upload_component_url(self, url, preview):
+        return super(PicovicoMusic, self)._upload_component_url(url, preview_url=preview)
