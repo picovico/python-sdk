434bb5804eff48db0a66415df21bb8ae9978b905
Sagar Chalise <chalisesagar@gmail.com>
2016-02-04 15:10:14 +0545

    Refactoring

--- lib/config.sample.py
+++
@@ -1,6 +0,0 @@
-'''
-        Picovico configuration for APP_ID, APP_SECRET and DEVICE_ID.
-'''
-PICOVICO_DEVICE_ID = 'com.picovico.api.python-sdk'
-PICOVICO_APP_ID = "some-app-id"
-PICOVICO_APP_SECRET = "some-app-secret"
--- setup.py
+++ setup.py
@@ -16,8 +16,18 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
+install_requires = [
+    'requests',
+    ]
+
+tests_require = [
+    'mock',
+    'pytest',
+    ]
+
 setup(
     name='picovico',
+    packages=['picovico'],
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
@@ -36,6 +46,8 @@ setup(
 
     # Choose your license
     license='MIT',
+    install_requires = install_requires,
+    tests_require = tests_require,
 
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
@@ -63,53 +75,8 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
+    setup_requires=['pytest-runner'],
 
     # What does your project relate to?
-    keywords='sample picovico slideshow-maker video-maker',
-
-    # You can just specify the packages manually here if your project is
-    # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-
-    # Alternatively, if you want to distribute just a my_module.py, uncomment
-    # this:
-    #   py_modules=["my_module"],
-
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn'],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={
-        'dev': ['check-manifest'],
-        'test': ['coverage'],
-    },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={
-        #'sample': ['package_data.dat'],
-    },
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-
-    #data_files=[('my_data', ['data/data_file'])],
-	data_files=[],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
-    entry_points={
-        #'console_scripts': [
-        #    'sample=sample:main',
-        #],
-    },
+    keywords='sample picovico slideshow-maker video-maker sdk',
 )
