14fa04c6021c7f75eb4e359ddc2de51efac9aa79
acpmasquerade <d@picovico.com>
2016-05-12 10:37:57 +0545

    abs import

--- picovico/__init__.py
+++ picovico/__init__.py
@@ -1,8 +1,10 @@
-from .session import PicovicoSessionMixin
-from .components import PicovicoComponentMixin
-from .decorators import pv_auth_required
-from . import urls as pv_urls
-from . import project as pv_project
+from __future__ import absolute_import
+
+from picovico.session import PicovicoSessionMixin
+from picovico.components import PicovicoComponentMixin
+from picovico.decorators import pv_auth_required
+from picovico import urls as pv_urls
+from picovico import project as pv_project
 
 
 class PicovicoAPI(PicovicoSessionMixin, PicovicoComponentMixin):
