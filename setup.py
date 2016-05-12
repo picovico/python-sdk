0de010307c37e8ef66ee3c1b0e9d3392d65b493f
acpmasquerade <d@picovico.com>
2016-05-12 12:51:31 +0545

    2.1.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='2.0.6',
+    version='2.1.0',
 
     description='Official Python SDK for Picovico Developer API',
     long_description=long_description,
@@ -56,7 +56,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
